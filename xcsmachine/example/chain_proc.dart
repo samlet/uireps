80dd8aea059bff3788e30ddcb1934e091e99cc9c
samlet <xiaofei.wu@gmail.com>
2024-09-08 08:59:03 +0800

    add chain/slab tests

---
+++ xcsmachine/example/chain_proc.dart
@@ -0,0 +1,14 @@
+import 'package:xcsmachine/src/calls/acl_on_chain.dart';
+import 'package:xcsmachine/src/calls/counter_comp.dart';
+import 'package:xcsmachine/src/calls/fixture_objects.dart';
+import 'package:xcsmachine/src/common/services/srv_base.dart';
+
+import 'jwt_proc.dart';
+
+var dio=createAuthDioWithToken(samletToken);
+Future<void> main(List<String> arguments) async {
+  var repo = AclOnChainRepository(dio);
+  var result = await repo.isOwner(biName: 'Note', bundleId: 'note_1', userOrGroup: 'samlet');
+  print("the result $result");
+}
+
---
+++ xcsmachine/example/slab_proc.dart
@@ -0,0 +1,12 @@
+import 'package:xcsmachine/src/calls/counter_comp.dart';
+import 'package:xcsmachine/src/calls/fixture_objects.dart';
+import 'package:xcsmachine/src/common/services/srv_base.dart';
+
+import 'jwt_proc.dart';
+
+var dio=createAuthDioWithToken(samletToken);
+Future<void> main(List<String> arguments) async {
+  var repo = FixtureObjectsRepository(dio);
+  var result = await repo.ping(req: 'hello');
+  print("the result $result");
+}
